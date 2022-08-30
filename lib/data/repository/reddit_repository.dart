198931e04c3d7736d7e227af75a265f932d34a0c
akbarjohnie <akbar.rashidov.2013@mail.ru>
2022-08-31 01:00:29 +0300

    changed model and repo, + fixed post screeb

--- lib/data/repository/reddit_repository.dart
+++ lib/data/repository/reddit_repository.dart
@@ -2,11 +2,26 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:reddit_posts/data/model/model.dart';
 
-class RedditDataRepositiry {
-  Future<RedditPostModel> getData() async {
+class RedditDataRepository {
+  Future<List<Post>> getData() async {
     final uri = Uri.parse('https://www.reddit.com/r/flutterdev/new.json');
     final response = await http.get(uri);
 
-    return RedditPostModel.fromJson(json.decode(response.body));
+    final Map<String, dynamic> data = json.decode(response.body);
+    final List children = data["data"]["children"];
+    final List<Map<String, dynamic>> targetData = children
+        .map((item) => ({
+              'selftext': item['data']['selftext'],
+              'ups': item['data']['ups'],
+              'thumbnail': item['data']['thumbnail'],
+              'title': item['data']['title']
+            }))
+        .toList();
+    List<Post> posts =
+        targetData.map((postData) => Post.fromJson(postData)).toList();
+
+    // print(posts.data?.children?[1].data?.title);
+
+    return posts;
   }
 }
---
+++ lib/style/text_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+const pview = TextStyle(
+  color: Colors.black,
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
